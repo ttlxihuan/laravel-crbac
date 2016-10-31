5fcb7faf4d819650f99cf0f433db59dff26b87ff
ttlxihuan <ttlxihuan@163.com>
2016-10-31 14:49:51 +0800

    Update RoleRelateEditTrait.php

--- src/Services/Power/RoleRelateEditTrait.php
+++ src/Services/Power/RoleRelateEditTrait.php
@@ -18,28 +18,34 @@ trait RoleRelateEditTrait {
      *       $allowAll 是否允许直接关联全部
      * 返回值：void
      */
-    protected function roleRelateEdit(Model $result, $relateClass, $allowAll = true) {
+    protected function roleRelateEdit(Model $result, $relateClass, $relationField, $allowAll = true) {
         $roles = Input::get('roles');
         if ($roles === 'all' && $allowAll) {//关联到所有角色
-            $role_ids = array_pluck(Role::get(['power_role_id']), 'power_role_id');
+            $role_ids = Role::get(['power_role_id'])
+                    ->pluck('power_role_id')
+                    ->toArray();
         } elseif ($roles) {//写入部分角色
-            $role_ids = array_pluck(Role::whereIn('power_role_id', explode(',', $roles))
-                            ->get(['power_role_id']), 'power_role_id');
+            $role_ids = Role::whereIn('power_role_id', explode(',', $roles))
+                    ->get(['power_role_id'])
+                    ->pluck('power_role_id')
+                    ->toArray();
         }
         if (isset($role_ids) && count($role_ids)) {//保存关联数据
-            $power_role_ids = array_pluck($relateClass::whereIn('power_role_id', $role_ids)//获取已有关联数据
-                            ->where($result->getKeyName(), '=', $result->getKey())
-                            ->get(['power_role_id']), 'power_role_id');
+            $power_role_ids = $relateClass::whereIn('power_role_id', $role_ids)//获取已有关联数据
+                    ->where($relationField, '=', $result->getKey())
+                    ->get(['power_role_id'])
+                    ->pluck('power_role_id')
+                    ->toArray();
             $relateClass::whereNotIn('power_role_id', $role_ids)//删除无需关联数据
-                    ->where($result->getKeyName(), '=', $result->getKey())
+                    ->where($relationField, '=', $result->getKey())
                     ->delete();
             $role_ids = array_diff($role_ids, $power_role_ids);
-            $inserts = array_map(function($item)use($result) {
-                return ['power_role_id' => $item, $result->getKeyName() => $result->getKey()];
+            $inserts = array_map(function($item)use($result, $relationField) {
+                return ['power_role_id' => $item, $relationField => $result->getKey()];
             }, $role_ids);
             $relateClass::insert($inserts);
         } else {//没有删除所有关联数据
-            $relateClass::where($result->getKeyName(), '=', $result->getKey())
+            $relateClass::where($relationField, '=', $result->getKey())
                     ->delete();
         }
     }
