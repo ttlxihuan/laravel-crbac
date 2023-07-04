34ee51d4f72e1e273e781c053a6b04def70f083e
xihuan <xihuan@adparticle.com>
2023-07-04 17:52:14 +0800

    排序样式

--- views/public/lists.blade.php
+++ views/public/lists.blade.php
@@ -2,6 +2,36 @@
 @if(isControllerPower('add') && !isset($not_add))
 @section('nimble')<a href="{{$add_url ?? crbac_route('.add')}}">新增{{$description}}</a>@stop
 @endif
+<style>
+    .order-by{
+        position: relative;
+        cursor: pointer;
+        box-sizing: border-box;
+        background-clip: padding-box;
+    }
+    .order-by:before,.order-by:after{
+        display:inline-block;
+        position: absolute;
+        right: 0px;
+        width: 20px;
+        height: 20px;
+        color: #9A9A9A;
+    }
+    .order-by:before{
+        content: '∧';
+        top:0px;
+    }
+    .order-by:after{
+        content: '∨';
+        bottom: 0px;
+    }
+    .order-desc:after{
+        color: #009900;
+    }
+    .order-asc:before{
+        color: #009900;
+    }
+</style>
 @section('main')
 @include('public.crumbs')
 <div class="container-fluid">
