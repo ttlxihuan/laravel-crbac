071405bdf96ec5b132b19cad7a599a5ecef7d8f6
ttlxihuan <ttlxihuan@163.com>
2024-10-30 16:27:58 +0800

    app-name

--- views/public/menu.blade.php
+++ views/public/menu.blade.php
@@ -1,7 +1,7 @@
 <!--菜单栏-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">包管理后台</a>
+        <a class="navbar-brand" href="#">{{config('app.name')}}管理后台</a>
         <button class="navbar-toggler" type="button">
             <span class="navbar-toggler-icon"></span>
         </button>
